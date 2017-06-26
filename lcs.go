f3072c43f94f5bf2692926b22c73f02790a80481
Jonathan Wills <runningwild@gmail.com>
2017-06-26 09:26:44 -0400

    working, need to speed up and test more

--- lcs.go
+++ lcs.go
@@ -3,9 +3,7 @@ package jig
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"sort"
-	"strings"
 )
 
 var (
@@ -13,94 +11,264 @@ var (
 	fileb = flag.String("b", "", "file b")
 )
 
+func stringToUint64s(s string) []uint64 {
+	var v []uint64
+	for _, r := range s {
+		v = append(v, uint64(r))
+	}
+	return v
+}
+
 func main() {
-	flag.Parse()
-	dataA, err := ioutil.ReadFile(*filea)
-	if err != nil {
-		panic(err)
-	}
-	dataB, err := ioutil.ReadFile(*fileb)
-	if err != nil {
-		panic(err)
-	}
-	linesA := strings.Split(string(dataA), "\n")
-	linesB := strings.Split(string(dataB), "\n")
-	v, max := stringsToUint64s([][]string{linesA, linesB})
-	mapping := make(map[uint64]uint64)
-	substrs := make(map[uint64]*substr)
-	for {
-		fmt.Printf("---------------------------------------------\n")
-		fmt.Printf("lengths: %d %d\n", len(v[0]), len(v[1]))
-		ai, bi, length := LCS(v[0], v[1])
-		if length <= 0 {
-			break
-		}
-		fmt.Printf("Pos(%d %d), Length %d\n", ai, bi, length)
-		v[0][ai] = max
-		v[1][bi] = max + 1
-		substrs[max] = &substr{start: ai, length: length}
-		substrs[max+1] = &substr{start: bi, length: length}
-		mapping[max] = max + 1
-		max += 2
+	in := stringToUint64s("abcdefghij")
+	skew := InducedSuffixArray(in)
+	dumb := DumbSuffixArray(in)
+	fmt.Printf("Skew: %v\n", skew)
+	fmt.Printf("Dumb: %v\n", dumb)
+}
 
-		for i := ai + 1; i+length-1 < len(v[0]); i++ {
-			v[0][i] = v[0][i+length-1]
-		}
-		v[0] = v[0][0 : len(v[0])-length+1]
-		for i := range v[0] {
-			fmt.Printf("%d: %d\n", i, v[0][i])
-		}
-
-		for i := bi + 1; i+length-1 < len(v[1]); i++ {
-			v[1][i] = v[1][i+length-1]
-		}
-		v[1] = v[1][0 : len(v[1])-length+1]
+// INDUCEDSUFFIXARRAY REQUIRES THAT NO ELEMENT IN S IS 0.
+// elements in s should be in the range [1, len(s)].
+func InducedSuffixArray(s []uint64) []int {
+	m := make(map[uint64]uint64)
+	for _, v := range s {
+		m[v]++
 	}
-	// liness := [][]string{linesA, linesB}
-	for i := range v {
-		add := 0
-		for j := range v[i] {
-			if s, ok := substrs[v[i][j]]; ok {
-				s.start += add
-				add += s.length
-				fmt.Printf("Lines %d %d: ", i, s.start)
-				// fmt.Printf("%s\n", liness[i][s.start])
+	var keys []uint64
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+	m = make(map[uint64]uint64)
+	for i, key := range keys {
+		m[key] = uint64(i + 1)
+	}
+	var T []uint64
+	for _, v := range s {
+		T = append(T, m[v])
+	}
+	return inducedSuffixArrayHelper(T)
+}
+
+func inducedSuffixArrayHelper(T []uint64) []int {
+	if len(T) == 0 {
+		return []int{}
+	}
+	// s = append(s, 0)
+	// SA := make([]int, len(s))
+	t := make([]plusOrMinusType, len(T)) // true indicates - type
+	t[len(t)-1] = minusType
+	for i := len(T) - 2; i >= 0; i-- {
+		if T[i] != T[i+1] {
+			if T[i] < T[i+1] {
+				t[i] = plusType
 			} else {
-				add++
+				t[i] = minusType
+			}
+		} else {
+			t[i] = t[i+1]
+		}
+	}
+	var P1 []int
+	prev := t[0]
+	for i := 1; i < len(t); i++ {
+		if t[i] == plusType && prev == minusType {
+			P1 = append(P1, i)
+		}
+		prev = t[i]
+	}
+
+	// TODO: this sort needs to be done faster
+	sort.Slice(P1, func(i, j int) bool {
+		i, j = P1[i], P1[j]
+		for k := 0; k+i < len(T) && k+j < len(T); k++ {
+			if T[k+i] != T[k+j] {
+				return T[k+i] < T[k+j]
+			}
+		}
+		return i > j
+	})
+	Cstar := make([][]int, len(T)+1)
+	for _, v := range P1 {
+		Cstar[int(T[v])] = append(Cstar[int(T[v])], v)
+	}
+
+	Cminus := InduceMinusSuffixes(T, Cstar, len(T)+1)
+	Cplus := InducePlusSuffixes(T, Cminus, len(T)+1)
+	var SA []int
+	for i := range Cminus {
+		SA = append(SA, Cminus[i]...)
+		SA = append(SA, Cplus[i]...)
+	}
+	return SA
+}
+
+type plusOrMinusType bool
+
+const plusType plusOrMinusType = true
+const minusType plusOrMinusType = false
+
+func InduceMinusSuffixes(T []uint64, Cstar [][]int, σ int) [][]int {
+	Cminus := make([][]int, σ)
+	Cminus[int(T[len(T)-1])] = append(Cminus[int(T[len(T)-1])], len(T)-1) // this line so ungly
+	// for _, a := range []int{'i', 'm', 'p', 's'} {
+	for a := 1; a < σ; a++ {
+		var c []int
+		for len(Cminus[a]) > 0 {
+			i := Cminus[a][0]
+			Cminus[a] = Cminus[a][1:]
+			c = append(c, i)
+			if i > 0 && T[i-1] >= uint64(a) {
+				Cminus[int(T[i-1])] = append(Cminus[int(T[i-1])], i-1)
+			}
+		}
+		Cminus[a] = c
+		for _, i := range Cstar[a] {
+			Cminus[int(T[i-1])] = append(Cminus[int(T[i-1])], i-1)
+		}
+	}
+	return Cminus
+}
+
+func InducePlusSuffixes(T []uint64, Cminus [][]int, σ int) [][]int {
+	Cplus := make([][]int, σ)
+	for a := σ - 1; a >= 1; a-- {
+		var c []int
+		for len(Cplus[a]) > 0 {
+			i := Cplus[a][len(Cplus[a])-1]
+			Cplus[a] = Cplus[a][0 : len(Cplus[a])-1]
+			c = append([]int{i}, c...)
+			if i > 0 && T[i-1] <= uint64(a) {
+				Cplus[int(T[i-1])] = append([]int{i - 1}, Cplus[int(T[i-1])]...)
+			}
+		}
+		Cplus[a] = c
+		for j := len(Cminus[a]) - 1; j >= 0; j-- {
+			i := Cminus[a][j]
+			if i > 0 && T[i-1] < uint64(a) {
+				Cplus[int(T[i-1])] = append([]int{i - 1}, Cplus[int(T[i-1])]...)
 			}
 		}
 	}
-	fmt.Printf("%v\n%v\n%v\n", v[0], v[1], mapping)
-	fmt.Printf("A:\n")
-	for _, v := range v[0] {
-		fmt.Printf("%d -> %v\n", v, substrs[v])
+	return Cplus
+}
+
+func boolsAsInts(b []plusOrMinusType) string {
+	var v string
+	for _, t := range b {
+		if t == plusType {
+			v += "+"
+		} else {
+			v += "-"
+		}
 	}
-	fmt.Printf("B:\n")
-	for _, v := range v[1] {
-		fmt.Printf("%d -> %v\n", v, substrs[v])
-	}
+	return v
+}
+
+func main2() {
+	// flag.Parse()
+	// dataA, err := ioutil.ReadFile(*filea)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// dataB, err := ioutil.ReadFile(*fileb)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// linesA := strings.Split(string(dataA), "\n")
+	// linesB := strings.Split(string(dataB), "\n")
+	// v, max := stringsToUint64s([][]string{linesA, linesB})
+	// mapping := make(map[uint64]uint64)
+	// substrs := make(map[uint64]*substr)
+	// for {
+	// 	fmt.Printf("---------------------------------------------\n")
+	// 	fmt.Printf("lengths: %d %d\n", len(v[0]), len(v[1]))
+	// 	ai, bi, length := LCS(v[0], v[1])
+	// 	if length <= 0 {
+	// 		break
+	// 	}
+	// 	fmt.Printf("Pos(%d %d), Length %d\n", ai, bi, length)
+	// 	v[0][ai] = max
+	// 	v[1][bi] = max + 1
+	// 	substrs[max] = &substr{start: ai, length: length}
+	// 	substrs[max+1] = &substr{start: bi, length: length}
+	// 	mapping[max] = max + 1
+	// 	max += 2
+
+	// 	for i := ai + 1; i+length-1 < len(v[0]); i++ {
+	// 		v[0][i] = v[0][i+length-1]
+	// 	}
+	// 	v[0] = v[0][0 : len(v[0])-length+1]
+	// 	for i := range v[0] {
+	// 		fmt.Printf("%d: %d\n", i, v[0][i])
+	// 	}
+
+	// 	for i := bi + 1; i+length-1 < len(v[1]); i++ {
+	// 		v[1][i] = v[1][i+length-1]
+	// 	}
+	// 	v[1] = v[1][0 : len(v[1])-length+1]
+	// }
+	// // liness := [][]string{linesA, linesB}
+	// for i := range v {
+	// 	add := 0
+	// 	for j := range v[i] {
+	// 		if s, ok := substrs[v[i][j]]; ok {
+	// 			s.start += add
+	// 			add += s.length
+	// 			fmt.Printf("Lines %d %d: ", i, s.start)
+	// 			// fmt.Printf("%s\n", liness[i][s.start])
+	// 		} else {
+	// 			add++
+	// 		}
+	// 	}
+	// }
+	// fmt.Printf("%v\n%v\n%v\n", v[0], v[1], mapping)
+	// fmt.Printf("A:\n")
+	// for _, v := range v[0] {
+	// 	fmt.Printf("%d -> %v\n", v, substrs[v])
+	// }
+	// fmt.Printf("B:\n")
+	// for _, v := range v[1] {
+	// 	fmt.Printf("%d -> %v\n", v, substrs[v])
+	// }
 }
 
 type substr struct {
 	start, length int
 }
 
-func stringsToUint64s(v [][]string) ([][]uint64, uint64) {
-	var ret [][]uint64
-	var count uint64
-	unique := make(map[string]uint64)
-	for i := range v {
-		var cur []uint64
-		for _, s := range v[i] {
-			if _, ok := unique[s]; !ok {
-				unique[s] = count
-				count++
-			}
-			cur = append(cur, unique[s])
-		}
-		ret = append(ret, cur)
+type runes []rune
+
+func (r runes) String() string {
+	var s string
+	for _, v := range r {
+		s += string(v) + " "
 	}
-	return ret, count
+	return "[" + s + "]"
+}
+func uint64sAsRunes(i []uint64) runes {
+	var s []rune
+	for _, v := range i {
+		s = append(s, rune(v))
+	}
+	return s
+}
+
+func DumbSuffixArray(v []uint64) []int {
+	r := make([]int, len(v))
+	for i := range r {
+		r[i] = i
+	}
+	sort.Slice(r, func(i, j int) bool {
+		i, j = r[i], r[j]
+		for k := 0; k+i < len(v) && k+j < len(v); k++ {
+			if v[i+k] != v[j+k] {
+				return v[i+k] < v[j+k]
+			}
+		}
+		return i > j // this just means the prefix starting at i is shorter
+	})
+	return r
 }
 
 // LCS finds the Longest Common Substring between two 'strings' of uint64.  ai and bi are the start
--- lcs_test.go
+++ lcs_test.go
@@ -1,6 +1,7 @@
 package jig_test
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/runningwild/jig"
@@ -8,69 +9,118 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestLCS(t *testing.T) {
-	Convey("LCS finds the longest common substring", t, func() {
-		Convey("at the start of one string and end of the other", func() {
-			ai, bi, length := jig.LCS(
-				toUint64s("abcdefghijklmnopqrstuvwxyz"),
-				toUint64s("012345678abcdefghijklmnopqrs"))
-			So(ai, ShouldEqual, 0)
-			So(bi, ShouldEqual, 9)
-			So(length, ShouldEqual, 19)
+func TestSuffixArray(t *testing.T) {
+	Convey("Induced algorithm works", t, func() {
+		Convey("on some canned inputs", func() {
+			tests := [][]uint64{
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+				{1, 2, 0, 1, 2, 0, 0, 1, 2, 0, 1, 1, 2, 2, 0, 1, 1, 2, 1, 1, 2, 2, 0},
+				{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+			}
+			for _, test := range tests {
+				got := jig.InducedSuffixArray(test)
+				want := jig.DumbSuffixArray(test)
+				So(got, ShouldResemble, want)
+			}
 		})
-		Convey("at the end of one string and start of the other", func() {
-			ai, bi, length := jig.LCS(
-				toUint64s("012345678abcdefghijklmnopqrs"),
-				toUint64s("abcdefghijklmnopqrstuvwxyz"))
-			So(ai, ShouldEqual, 9)
-			So(bi, ShouldEqual, 0)
-			So(length, ShouldEqual, 19)
+		Convey("on medium length inputs with small alphabets", func() {
+			rng := rand.New(rand.NewSource(123))
+			for i := 0; i < 1000; i++ {
+				length := rng.Intn(1000) + 100
+				alphabet := rng.Intn(5) + 2
+				input := make([]uint64, length)
+				for i := range input {
+					input[i] = uint64(rng.Intn(alphabet))
+				}
+				got := jig.InducedSuffixArray(input)
+				want := jig.DumbSuffixArray(input)
+				So(got, ShouldResemble, want)
+			}
 		})
-		Convey("when the strings are equal", func() {
-			ai, bi, length := jig.LCS(
-				toUint64s("abcdefghijklmnopqrstuvwxyz"),
-				toUint64s("abcdefghijklmnopqrstuvwxyz"))
-			So(ai, ShouldEqual, 0)
-			So(bi, ShouldEqual, 0)
-			So(length, ShouldEqual, 26)
-		})
-		Convey("when the strings have no common values", func() {
-			_, _, length := jig.LCS(
-				toUint64s("abcdefghijklmnopqrstuvwxyz"),
-				toUint64s("0123456789"))
-			So(length, ShouldEqual, 0)
+		Convey("on some random long inputs", func() {
+			rng := rand.New(rand.NewSource(123))
+			for i := 0; i < 100; i++ {
+				length := rng.Intn(1000) + 100
+				alphabet := rng.Intn(500) + 2
+				input := make([]uint64, length)
+				for i := range input {
+					input[i] = uint64(rng.Intn(alphabet))
+				}
+				got := jig.InducedSuffixArray(input)
+				want := jig.DumbSuffixArray(input)
+				So(got, ShouldResemble, want)
+			}
 		})
 	})
 }
 
-func toUint64s(s string) []uint64 {
-	var v []uint64
-	for _, r := range s {
-		v = append(v, uint64(r))
-	}
-	return v
-}
+// func TestLCS(t *testing.T) {
+// 	Convey("LCS finds the longest common substring", t, func() {
+// 		Convey("at the start of one string and end of the other", func() {
+// 			ai, bi, length := jig.LCS(
+// 				toUint64s("abcdefghijklmnopqrstuvwxyz"),
+// 				toUint64s("012345678abcdefghijklmnopqrs"))
+// 			So(ai, ShouldEqual, 0)
+// 			So(bi, ShouldEqual, 9)
+// 			So(length, ShouldEqual, 19)
+// 		})
+// 		Convey("at the end of one string and start of the other", func() {
+// 			ai, bi, length := jig.LCS(
+// 				toUint64s("012345678abcdefghijklmnopqrs"),
+// 				toUint64s("abcdefghijklmnopqrstuvwxyz"))
+// 			So(ai, ShouldEqual, 9)
+// 			So(bi, ShouldEqual, 0)
+// 			So(length, ShouldEqual, 19)
+// 		})
+// 		Convey("when the strings are equal", func() {
+// 			ai, bi, length := jig.LCS(
+// 				toUint64s("abcdefghijklmnopqrstuvwxyz"),
+// 				toUint64s("abcdefghijklmnopqrstuvwxyz"))
+// 			So(ai, ShouldEqual, 0)
+// 			So(bi, ShouldEqual, 0)
+// 			So(length, ShouldEqual, 26)
+// 		})
+// 		Convey("when the strings have no common values", func() {
+// 			_, _, length := jig.LCS(
+// 				toUint64s("abcdefghijklmnopqrstuvwxyz"),
+// 				toUint64s("0123456789"))
+// 			So(length, ShouldEqual, 0)
+// 		})
+// 	})
+// }
 
-func benchmark(b *testing.B, lenA, lenB, overlap int) {
-	b.StopTimer()
-	ab := make([]uint64, lenA+lenB-overlap)
-	for i := range ab {
-		ab[i] = uint64(i)
-	}
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		jig.LCS(ab[0:lenA], ab[overlap:])
-	}
-}
+// func toUint64s(s string) []uint64 {
+// 	var v []uint64
+// 	for _, r := range s {
+// 		v = append(v, uint64(r))
+// 	}
+// 	return v
+// }
 
-func BenchmarkLCS_100_100_halfequal(b *testing.B) {
-	benchmark(b, 100, 100, 50)
-}
+// func benchmark(b *testing.B, lenA, lenB, overlap int) {
+// 	b.StopTimer()
+// 	ab := make([]uint64, lenA+lenB-overlap)
+// 	for i := range ab {
+// 		ab[i] = uint64(i)
+// 	}
+// 	b.StartTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		jig.LCS(ab[0:lenA], ab[overlap:])
+// 	}
+// }
 
-func BenchmarkLCS_1000_1000_halfequal(b *testing.B) {
-	benchmark(b, 1000, 1000, 500)
-}
+// func BenchmarkLCS_100_100_halfequal(b *testing.B) {
+// 	benchmark(b, 100, 100, 50)
+// }
 
-func BenchmarkLCS_10000_10000_halfequal(b *testing.B) {
-	benchmark(b, 10000, 10000, 5000)
-}
+// func BenchmarkLCS_1000_1000_halfequal(b *testing.B) {
+// 	benchmark(b, 1000, 1000, 500)
+// }
+
+// func BenchmarkLCS_10000_10000_halfequal(b *testing.B) {
+// 	benchmark(b, 10000, 10000, 5000)
+// }
